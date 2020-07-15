750887d87515ecc8dc7da969312417908e40d33d
Brandon Hendrix <bdhendrix@gmail.com>
2020-07-14 17:58:38 -0700

    initial

---
+++ www/src/controller.js
@@ -0,0 +1,26 @@
+import {Movement} from 'wasm-snake-game'
+
+const MOVEMENT_KEYS = {
+    [Movement.TOP]: [87, 38],
+    [Movement.RIGHT]: [68, 39],
+    [Movement.DOWN]: [83, 40],
+    [Movement.LEFT]: [65, 37]
+}
+
+const STOP_KEY = 32
+
+export class Controller {
+    constructor(onStop = () => {}) {
+        window.addEventListener('keydown', ({ which }) => {
+            this.movement = Object.keys(MOVEMENT_KEYS).find(key => MOVEMENT_KEYS[key].includes(which));
+            console.log(this.movement);
+        });
+        window.addEventListener('keyup', ({ which }) => {
+            this.movement = undefined;
+            console.log(this.movement);
+            if (which === STOP_KEY) {
+                onStop();
+            }
+        });
+    }
+}
